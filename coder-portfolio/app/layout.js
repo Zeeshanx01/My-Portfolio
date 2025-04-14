3e7d161b620e38551a7276249496bdcaca891a0c
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2025-04-14 12:55:13 +0500

    Update layout.js

--- coder-portfolio/app/layout.js
+++ coder-portfolio/app/layout.js
@@ -4,6 +4,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
         >
           {children}
           <SpeedInsights />
+          <Analytics />
         </body>
     </html>
   );
