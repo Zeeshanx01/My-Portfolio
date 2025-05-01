99a8ea2c90023afa20126d682705babbef95a26d
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2025-05-01 19:19:25 +0500

    test-2.2

--- coder-portfolio/app/page.js
+++ coder-portfolio/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <div className={`min-h-screen b-black bg-opacity-20 text-gray-300 ${fontStyles.body}`}>
       
-      {/* <Particles /> */}
+      <Particles />
       {/* <Cursor /> */}
 
 
